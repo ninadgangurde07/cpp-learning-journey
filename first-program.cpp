c2b5ab09692dcd7e68d2c6a1bdcf1dd566c9802a
Ninad Nitin Gangurde <ninadgangurde07@gmail.com>
2026-09-15 14:27:08 +0530

    Enhance first-program.cpp with comments
    Added comments to explain code functionality and variable declarations.

--- first-program.cpp
+++ first-program.cpp
@@ -1,9 +1,22 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Ninad Gangurde\n";
+#include<iostream>//allows to use input and output functions
+using namespace std;//allows to use standard library functions
+int main(){//main function is the starting point of the program
+
+    cout<<"Ninad Gangurde\n";//this will print my name on the screen
     
 //THIS IS MY FIRST PROGRAM 
-cout<<"THE WORLD IS YOURS"<<endl;
+cout<<"THE WORLD IS YOURS\n";
+int Age=19;//this is integer variable which stores my age
+char Gender='M';//this is character variable which stores my gender
+float Height=5.10;//this is float variable which stores my height
+bool isadult=true;//this is boolean variable which stores whether I am an adult
+double Weight=75.13;//this is double variable which stores my weight
+cout<<"Age:"<<Age<<"\n";
+cout<<"Gender:"<<Gender<<"\n";
+cout<<"Height:"<<Height<<"\n";
+cout<<"Is Adult:"<<isadult<<"\n";
+cout<<"Weight:"<<Weight<<"\n";
+//the upper part of the code is used to declare variables and print them on the screen
+
 return 0 ;
 }
